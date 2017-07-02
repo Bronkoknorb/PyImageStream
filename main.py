7045ad7810f8349c051382b3414d8f0d97655ae4
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2017-07-02 11:45:23 +0200

    Delay stopping the camera. Calculate and display FPS in client.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 # Configuration
-JPEG_QUALITY = 75
+JPEG_QUALITY = 70
 CAMERA_INDEX = 0
 WEBSERVER_PORT = 8888
 WIDTH = 1920
 HEIGHT = 1080
+
+# Delay stopping the camera after all clients have disconnected
+DELAY_CAMERA_STOP_SECONDS = 7
 
 import tornado.ioloop
 import tornado.web
@@ -29,18 +32,34 @@ class Camera:
         self._cam = pygame.camera.Camera(camera_name, (WIDTH, HEIGHT))
         print("Camera initialized")
         self.is_started = False
+        self.stop_requested = False
 
-    def start(self):
+    def request_start(self):
+        if self.stop_requested:
+            print("Camera continues to be in use")
+            self.stop_requested = False
+        if not self.is_started:
+            self._start()
+
+    def request_stop(self):
+        if self.is_started and not self.stop_requested:
+            self.stop_requested = True
+            print("Stopping camera in " + str(DELAY_CAMERA_STOP_SECONDS) + " seconds...")
+            tornado.ioloop.IOLoop.current().call_later(DELAY_CAMERA_STOP_SECONDS, self._stop)
+
+    def _start(self):
         print("Starting camera...")
         self._cam.start()
         print("Camera started")
         self.is_started = True
 
-    def stop(self):
-        print("Stopping camera...")
-        self._cam.stop()
-        print("Camera stopped")
-        self.is_started = False
+    def _stop(self):
+        if self.stop_requested:
+            print("Stopping camera now...")
+            self._cam.stop()
+            print("Camera stopped")
+            self.is_started = False
+            self.stop_requested = False
 
     def get_jpeg_image_bytes(self):
         img = self._cam.get_image()
@@ -60,8 +79,7 @@ class ImageWebSocket(tornado.websocket.WebSocketHandler):
     def open(self):
         ImageWebSocket.clients.add(self)
         print("WebSocket opened from: " + self.request.remote_ip)
-        if not camera.is_started:
-            camera.start()
+        camera.request_start()
 
     def on_message(self, message):
         jpeg_bytes = camera.get_jpeg_image_bytes()
@@ -70,8 +88,8 @@ class ImageWebSocket(tornado.websocket.WebSocketHandler):
     def on_close(self):
         ImageWebSocket.clients.remove(self)
         print("WebSocket closed from: " + self.request.remote_ip)
-        if camera.is_started and len(ImageWebSocket.clients) == 0:
-            camera.stop()
+        if len(ImageWebSocket.clients) == 0:
+            camera.request_stop()
 
 
 script_path = os.path.dirname(os.path.realpath(__file__))
@@ -82,4 +100,7 @@ app = tornado.web.Application([
         (r"/(.*)", tornado.web.StaticFileHandler, {'path': static_path, 'default_filename': 'index.html'}),
     ])
 app.listen(WEBSERVER_PORT)
+
+print("Starting server: http://localhost:" + str(WEBSERVER_PORT) + "/")
+
 tornado.ioloop.IOLoop.current().start()
